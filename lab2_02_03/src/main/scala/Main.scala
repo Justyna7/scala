14fe4c04beefd9993300f967e5f255203ef7d49c
Justyna <justiluc@interia.pl>
2021-03-04 18:27:33 +0100

    lab 2 zad 2

--- lab2_02_03/src/main/scala/Main.scala
+++ lab2_02_03/src/main/scala/Main.scala
@@ -1,20 +1,4 @@
 object Main extends App {
-  // println("Jeśli koniecznie chcemy, to w Scali możemy też pisać w stylu XIX-wiecznym:")
-  // print("Podaj wartość y: ")
-  // // Wczytujemy liczbę całkowitą i zapamiętujemy ją jako „wartość” max
-  // // Użycie wartości zamiast zmiennej to drobne odstępstwo od stylu XIX-wiecznego ;)
-  // val max = io.StdIn.readInt()
-  // // deklarujemy i inicjalizujemy zmienną x
-  // var x = 1
-  // // drobna próbka „interpolacji” w wypisywanym napisie
-  // println(s"Liczby z przedziału [1..$max] to:")
-  // // w „pętli” zmieniamy wartość zmiennej aż będzie ona równa „max”
-  // while (x <= max) {
-  //   println(x)
-  //   x += 1
-  // }
-
-
   // Zad 1
   // Napisz program, który wczytuje ze standardowego wejścia liczbę naturalną (patrz Uwaga poniżej),
   // a następnie sprawdza czy jest ona liczbą pierwszą i wyświetla odpowiednią informację.
@@ -38,4 +22,40 @@ object Main extends App {
   }
   val liczba = io.StdIn.readInt()
   println(czy_pierwsza(liczba))
+
+// Zad 2
+// Napisz funkcję rekurencyjną jestPalindromem(tab: Array[Int]): Boolean,
+// która sprawdza czy tablica będąca jej argumentem jest palindromem.
+// Dostęp do elementu tablicy tab o indeksie i uzyskujemy przez tab(i).
+// Elementy tablicy mają indeksy od 0 do tab.size - 1.
+// Przykładowa tablica, która jest palindromem to Array(1,2,3,2,1).
+// Rozwiąż to zadanie bez korzystania ze zmiennych i "pętli".
+
+def jestPalindromem(tab: Array[Int]): Boolean = {
+  @annotation.tailrec
+    def poboczna(tab: Array[Int], i: Int, pol: Int, s: Int): Boolean = {
+      if (tab(i) == tab(s-i-1)){
+        if (i == pol) true
+        else poboczna(tab, i+1, pol, s)
+      }else false
+    }
+    if (tab.length <= 1) true
+    else {
+      val s = tab.length
+      poboczna(tab, 0, s/2, s)
+    }
+  }
+val palindrom = Array(1)
+val palindrom2 = Array(1,2)
+val palindrom3 = Array(1,1)
+val palindrom4 = Array(1,2,1)
+val palindrom5 = Array(1,2,2,1)
+val palindrom6 = Array(1,2,4,1)
+
+println(jestPalindromem(palindrom))
+println(jestPalindromem(palindrom2))
+println(jestPalindromem(palindrom3))
+println(jestPalindromem(palindrom4))
+println(jestPalindromem(palindrom5))
+println(jestPalindromem(palindrom6))
 }
