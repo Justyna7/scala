c012076895bc9491c50d526fc644f8264fa3d4cf
Justyna <justiluc@interia.pl>
2021-03-19 18:28:25 +0100

    lab 4 zad 2

--- lab4_16_03/src/main/scala/Main.scala
+++ lab4_16_03/src/main/scala/Main.scala
@@ -18,4 +18,45 @@ def sumuj(l: List[Option[Double]]): Option[Double] = {
 println(sumuj(List(Some(2.0), Some(4.0), Some(-3.0), None, Some(-3.0), None, Some(1.0))))
 println(sumuj(List(None)))
 println(sumuj(List()))
+
+// Zadanie 2.
+// Zdefiniuj funkcję rekurencyjną:
+// def tasuj(l1: List[Int], l2: List[Int]): List[Int]
+// która łączy ze sobą dwie listy liczb całkowitych z zachowaniem porządku.
+// W szczególności oznacza to, że jeśli l1 i l2 będą uporządkowane to wynik również będzie uporządkowany.
+// W wyniku nie powinny pojawiać się kolejno powtarzające się elementy.
+// Rozwiąż to zadanie wykorzystując rekurencję ogonową i dopasowanie wzorców (nie używaj metod head i tail).
+// Przykład:
+// Dla: tasuj(List(2, 4, 3, 5), List(1, 2, 2, 3, 1, 5)), funkcja powinna zwrócić: List(1, 2, 3, 1, 4, 3, 5).
+def tasuj(l1: List[Int], l2: List[Int]): List[Int]={
+  def poboczna(l1: List[Int], l2: List[Int], w: List[Int]): List[Int] = (l1,l2) match {
+    case (List(),List()) => w.reverse
+    case (List(),a::b) => w match {
+                                    case x::_ if x == a => poboczna(List(), b, w)
+                                    case _ => poboczna(List(), b, a::w)
+                                  }
+    case (a::b,List()) => w match {
+                                    case x::_ if x == a => poboczna(b, List(), w)
+                                    case _ => poboczna(b, List(), a::w)
+                                  }
+    case (a::b, c::d) if (c>a) => w match {
+                                    case x::_ if x == a => poboczna(b, c::d, w)
+                                    case _ => poboczna(b,c::d, a::w)
+                                  }
+    case (a::b, c::d) if (c==a) => w match {
+                                    case x::_ if x == a => poboczna(b, d, w)
+                                    case _ => poboczna(b,d, a::w)
+                                  }
+    case (a::b, c::d) => w match {
+                            case x::_ if x == c => poboczna(a::b, d, w)
+                            case _ => poboczna(a::b,d, c::w)
+                          }
+  }
+  poboczna(l1,l2,List())
+}
+println(tasuj(List(), List()))
+println(tasuj(List(2, 4, 3, 5), List(1, 2, 2, 3, 1, 5)))
+
+
+
 }
