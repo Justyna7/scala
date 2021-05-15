9eeaa026ba694afa943d798b8a1cf1e5a68dd9e7
Justyna <justiluc@interia.pl>
2021-05-15 02:17:26 +0200

    lab 10 zad 2

--- lab10_04_05/src/main/scala/Zad2.scala
+++ lab10_04_05/src/main/scala/Zad2.scala
@@ -7,8 +7,36 @@ object Zad2 {
   // tak, aby po utworzeniu trzech aktorów typu Gracz mogli oni grać w "ping-ponga" w trójkącie.
   // Jako "piłeczki" użyj obiektu
   // case object Piłeczka
+  case object Piłeczka
+  case class Graj(a:ActorRef, p:Boolean)
+  class Gracz extends Actor { 
+    def receive: Receive = {
+      case Graj(a, p) =>{
+        println(""+ self.path.name + " rozpoczynam gre")
+        context.become(graj(a, p))
+      }
+    }
+    def graj(a:ActorRef, ping: Boolean): Receive = {
+      //case Graj => a ! Piłeczka
+      case Piłeczka => {
+        ping match{
+          case true => println(self.path.name + " ping")
+          case false => println(self.path.name + " pong")
+        }
+        context.become(graj(a, !ping))
+        a ! Piłeczka
+      }
+    }
+   }
   def main(args: Array[String]): Unit = {
-    ???
+    val system = ActorSystem("sys")
+    val gracz1 = system.actorOf(Props[Gracz], "gracz1")
+    val gracz2 = system.actorOf(Props[Gracz], "gracz2")
+    val gracz3 = system.actorOf(Props[Gracz], "gracz3")
+    gracz1 ! Graj(gracz2, true)
+    gracz2 ! Graj(gracz3, false)
+    gracz3 ! Graj(gracz1, true)
+    gracz1 ! Piłeczka
   }
-
+  
 }
